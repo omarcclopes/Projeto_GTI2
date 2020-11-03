facba9f02c46415b1e14ced6ae7651553afe9a65
omarcclopes <69813033+omarcclopes@users.noreply.github.com>
2020-11-03 14:00:58 -0300

    ok01
    Até aqui OK

--- projeto/eventosfacet/gestoreventos/admin.py
+++ projeto/eventosfacet/gestoreventos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from gestoreventos.models import Curso, Funcionario
 
 # Register your models here.
+admin.site.register(Curso)
+admin.site.register(Funcionario)
--- projeto/eventosfacet/gestoreventos/models.py
+++ projeto/eventosfacet/gestoreventos/models.py
@@ -8,3 +8,19 @@ class Evento(models.Model):
     def __str__(self):
         return self.nome_evento
         
+class Curso(models.Model):
+    nome_curso = models.CharField(max_length=100)
+    codigo_curso = models.IntegerField()
+
+    def __str__(self):
+        return self.nome_curso
+
+class Funcionario(models.Model):
+    nome_funcionario = models.CharField(max_length=150)   
+    matricula = models.IntegerField()
+    email_institucional = models.CharField(max_length=100)
+    curso_vinculado = models.ForeignKey(Curso, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.nome_funcionario
+        
