5013fc78c9b6ed9e38ec7d1010eb361c37fcaa90
omarcclopes <69813033+omarcclopes@users.noreply.github.com>
2020-11-03 22:28:49 -0300

    Funcionando solução Omar
    Funcionando solução Omar

--- projeto/eventosfacet/gestoreventos/admin.py
+++ projeto/eventosfacet/gestoreventos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from gestoreventos.models import Curso, Funcionario
+from gestoreventos.models import Curso, Funcionario, Palestrante, Evento
 
 # Register your models here.
 admin.site.register(Curso)
 admin.site.register(Funcionario)
+admin.site.register(Palestrante)
+admin.site.register(Evento)
--- projeto/eventosfacet/gestoreventos/models.py
+++ projeto/eventosfacet/gestoreventos/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Evento(models.Model):
-    nome_evento = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.nome_evento
-        
 class Curso(models.Model):
     nome_curso = models.CharField(max_length=100)
     codigo_curso = models.IntegerField()
@@ -19,8 +13,29 @@ class Funcionario(models.Model):
     nome_funcionario = models.CharField(max_length=150)   
     matricula = models.IntegerField()
     email_institucional = models.CharField(max_length=100)
-    curso_vinculado = models.ForeignKey(Curso, on_delete=models.RESTRICT)
+    curso_vinculado = models.ForeignKey(Curso, on_delete=models.RESTRICT, blank=True, null=True)
 
     def __str__(self):
         return self.nome_funcionario
+
+class Palestrante(models.Model):
+    nome_palestrante = models.CharField(max_length=150)
+    data_nascimento = models.DateField(auto_now=False, auto_now_add=False)
+    cpf = models.CharField(max_length=11)
+    email_palestrante = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome_palestrante
+
+class Evento(models.Model):
+    nome_evento = models.CharField(max_length=200)
+    data_inicio = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    data_final = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    curso_organizador = models.ForeignKey(Curso, on_delete=models.RESTRICT, blank=True, null=True)
+    palestrantes = models.ForeignKey(Palestrante, on_delete=models.RESTRICT, blank=True, null=True)
+    organizador = models.ForeignKey(Funcionario, on_delete=models.RESTRICT, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome_evento
         
