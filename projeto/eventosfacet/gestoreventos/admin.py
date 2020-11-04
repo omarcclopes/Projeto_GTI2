2cbd0666285cf4c161ee80bf02628b045e3027d8
omarcclopes <69813033+omarcclopes@users.noreply.github.com>
2020-11-03 23:18:30 -0300

    fuso horario
    fuso horario

--- projeto/eventosfacet/gestoreventos/admin.py
+++ projeto/eventosfacet/gestoreventos/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+#from gestoreventos.models import Curso, Funcionario, Palestrante, Evento
 from gestoreventos.models import Curso, Funcionario, Palestrante, Evento
 
 # Register your models here.
